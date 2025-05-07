2019e39db1730a67f104579e57794a25b13f8496
PH <paulofera159@gmail.com>
2025-05-07 13:47:07 -0300

    Atualização

--- src/services/serviceLogin.js
+++ src/services/serviceLogin.js
@@ -28,6 +28,7 @@ async function login (req, res) {
       findCandidato.password
     )
     if (!verifyPassword) {
+      console.log('teste')
       return answers.badRequest(res, 'Email ou Senha incorretos!')
     } else {
       const token = jwt.sign(
@@ -44,6 +45,7 @@ async function login (req, res) {
       return answers.success(res, token)
     }
   } catch (error) {
+    console.log('teste')
     return answers.internalServerError(res, 'Erro ao efetuar o login', error)
   }
 }
