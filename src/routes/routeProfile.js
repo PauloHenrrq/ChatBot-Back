ac9ba7ef6353e41fcdf960c8fbed90a8737d62e4
PH <paulofera159@gmail.com>
2025-05-07 16:39:19 -0300

    Criação da rota Profile | Fazer verificação da role

--- src/routes/routeProfile.js
+++ src/routes/routeProfile.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import authMiddleware from "../middleware/authMiddleware";
+import authMiddleware from '../middleware/authMiddleware';
 
 import controllerGetProfile from "../controller/controllerProfile";
 const getProfile = controllerGetProfile
