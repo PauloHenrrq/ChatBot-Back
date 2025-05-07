5819a66de3cde66b90640c13efd87d15f2fcfc55
PH <paulofera159@gmail.com>
2025-05-07 12:09:59 -0300

    Atualização Final - Railway

--- src/database/database.js
+++ src/database/database.js
@@ -8,11 +8,17 @@ const DBuser = process.env.DB_USER
 const DBpassword = process.env.DB_PASSWORD
 const DBport = process.env.DB_PORT
 
+console.log('DB_HOST:', DBhost)
+console.log('DB_NAME:', DBname)
+console.log('DB_USER:', DBuser)
+console.log('DB_PASSWORD:', DBpassword)
+console.log('DB_PORT:', DBport)
+
 const DBConnection = new Sequelize(DBname, DBuser, DBpassword, {
   host: DBhost,
   dialect: 'mysql',
   port: DBport,
-  logging: false
+  logging: false,
 })
 
 const testConnection = async () => {
