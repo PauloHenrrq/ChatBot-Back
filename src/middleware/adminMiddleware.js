1d229addd1ca1785d65ae6415cca7df02e42f58a
PH <paulofera159@gmail.com>
2025-05-08 13:11:23 -0300

    Att

--- src/middleware/adminMiddleware.js
+++ src/middleware/adminMiddleware.js
@@ -1,7 +1,7 @@
 import answers from '../responses.js'
 
 const adminMiddleware = (req, res, next) => {
-  console.log('req.user:', req.user)
+  console.log('req.user:', req.user.data)
   if (req.user.role != 'admin') {
     return answers.unauthorized(
       res,
