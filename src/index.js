94d9d5977820ab1671cf27c0467e67cd77b5b622
PH <paulofera159@gmail.com>
2025-05-08 00:12:33 -0300

    Att

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import candidaturaRoute from './routes/routeCandidatura.js'
 import loginRoute from './routes/routeLogin.js'
 import vagaRoute from './routes/routeVaga.js'
 import profileRoute from './routes/routeProfile.js'
+import Vaga from './models/Vaga.js'
 
 const app = express()
 app.use(cors())
@@ -15,5 +16,26 @@ app.use(candidaturaRoute)
 app.use(vagaRoute)
 app.use(loginRoute)
 app.use(profileRoute)
+
+app.post('/teste-vaga', async (req, res) => {
+  try {
+    const vaga = await Vaga.create({
+      titulo: 'Teste',
+      empresa: 'Teste',
+      cep: '12345678',
+      descricao: 'Descrição teste',
+      requisitos: ['Requisito 1'],
+      responsabilidades: ['Responsabilidade 1'],
+      beneficios: ['Benefício 1'],
+      salario: 'R$ 2000',
+      informacoes_adicionais: 'Nada'
+    })
+    res.status(201).json(vaga)
+  } catch (error) {
+    console.error(error.message)
+    console.error(error.stack)
+    res.status(500).json({ erro: error.message })
+  }
+})
 
 export default app
