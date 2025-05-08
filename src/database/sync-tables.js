591ef93b511332496eb7297aeacbcb632677b40b
PH <paulofera159@gmail.com>
2025-05-07 23:08:44 -0300

    Atualização BD

--- src/database/sync-tables.js
+++ src/database/sync-tables.js
@@ -1,16 +1,16 @@
-import AssociationsKeys from "../models/AssociationsKeys.js";
-import Candidato from "../models/Candidato.js";
-import Candidatura from "../models/Candidatura.js";
-import Vaga from "../models/Vaga.js";
-import DBConnection from "./database.js";
+import AssociationsKeys from '../models/AssociationsKeys.js'
+import DBConnection from './database.js'
 
 AssociationsKeys()
 
 const syncTables = async () => {
-  await DBConnection.sync(/*{ alter: true }*/);
-  await Candidato.sync(/*{ alter: true }*/);
-  await Vaga.sync();
-  await Candidatura.sync();
-};
+  try {
+    await DBConnection.sync({ alter: true })
 
-export default syncTables;
+    console.log('Tabelas sincronizadas com sucesso!')
+  } catch (error) {
+    console.error('Erro ao sincronizar tabelas:', error)
+  }
+}
+
+export default syncTables
