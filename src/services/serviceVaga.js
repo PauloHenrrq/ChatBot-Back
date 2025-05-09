6c5bfcfdbcebc5d86298bba0596598381e92a107
PH <paulofera159@gmail.com>
2025-05-08 23:56:37 -0300

    Att

--- src/services/serviceVaga.js
+++ src/services/serviceVaga.js
@@ -175,29 +175,33 @@ async function putVaga (req, res) {
   }
 }
 
-async function deleteVaga(req, res) {
+async function deleteVaga (req, res) {
   try {
-    const { id } = req.params;
+    const { id } = req.params
 
     const vagaCheck = await Vaga.findOne({
       where: {
         id: id
       }
-    });
+    })
 
-    const vagaDestroyed = await Vaga.destroy({
+    if (!vagaCheck || vagaCheck.length === 0) {
+      return answers.badRequest(res, 'Essa vaga não existe')
+    }
+
+    Vaga.destroy({
       where: {
         id: id
       }
-    });
+    })
 
-    return answers.success(res, 'Vaga deletada com sucesso', vagaDestroyed);
+    return answers.success(res, 'Vaga deletada com sucesso', vagaDestroyed)
   } catch (error) {
     return answers.internalServerError(
       res,
       'Houve um erro ao deletar a vaga',
       error
-    );
+    )
   }
 }
 
