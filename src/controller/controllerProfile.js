743b6068b0714888d6f979c15c1c8537d6ebd3f7
PH <paulofera159@gmail.com>
2025-05-11 00:42:36 -0300

    Remoção do Profile

--- src/controller/controllerProfile.js
+++
@@ -1,7 +0,0 @@
-import getProfile from "../services/serviceProfile.js";
-
-const controllerProfile = (req, res) => {
-    getProfile (req, res)
-}
-
-export default controllerProfile
--- src/services/serviceProfile.js
+++
@@ -1,9 +0,0 @@
-import answers from "../responses.js";
-
-async function getProfile (req, res) {
-    const role = req.user.role
-
-    return answers.success(res, { role });
-}
-
-export default getProfile
