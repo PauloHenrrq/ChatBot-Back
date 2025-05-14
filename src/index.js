25d3d82db20e68cee30db9cd454ad3c0c7ba2772
PH <paulofera159@gmail.com>
2025-05-14 11:47:00 -0300

    Autorização de rota CORS

--- src/index.js
+++ src/index.js
@@ -10,10 +10,12 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use({
-  origin: ['http://localhost:5173'],
-  credentials: true
-})
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+  })
+)
 
 app.use(candidatoRoute)
 app.use(candidaturaRoute)
