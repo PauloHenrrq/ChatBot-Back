33dbedc5ff025a4c55c37dec48ddbd21660ccd2f
PH <paulofera159@gmail.com>
2025-05-14 11:51:28 -0300

    Voltando commit

--- src/index.js
+++ src/index.js
@@ -8,13 +8,6 @@ import notificacaoRoute from './routes/routeNotificacao.js'
 
 const app = express()
 app.use(cors())
-app.use(
-  cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-  })
-)
-
 app.use(express.json())
 
 app.use(candidatoRoute)
