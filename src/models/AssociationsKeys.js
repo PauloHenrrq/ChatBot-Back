3bf36ad93a7bc7abd2e583078bbb58470f8c8ee1
PH <paulofera159@gmail.com>
2025-05-14 08:43:44 -0300

    Relação de Candidato => Notificacao

--- src/models/AssociationsKeys.js
+++ src/models/AssociationsKeys.js
@@ -1,10 +1,15 @@
-import Candidato from '../models/Candidato.js';
-import Candidatura from '../models/Candidatura.js';
-import Vaga from '../models/Vaga.js';
+import Candidato from '../models/Candidato.js'
+import Candidatura from '../models/Candidatura.js'
+import Vaga from '../models/Vaga.js'
+import Notificacao from './Notificacao.js'
 
-Vaga.hasMany(Candidatura, { foreignKey: 'vagaId' });
-Candidatura.belongsTo(Vaga, { foreignKey: 'vagaId' });
-Candidato.hasMany(Candidatura, { foreignKey: 'candidatoId' });
-Candidatura.belongsTo(Candidato, { foreignKey: 'candidatoId' });
+Vaga.hasMany(Candidatura, { foreignKey: 'vagaId' })
+Candidatura.belongsTo(Vaga, { foreignKey: 'vagaId' })
 
-export default () => {}; // Pode exportar uma função vazia
+Candidato.hasMany(Candidatura, { foreignKey: 'candidatoId' })
+Candidatura.belongsTo(Candidato, { foreignKey: 'candidatoId' })
+
+Candidato.hasMany(Notificacao, { foreignKey: 'userId' })
+Notificacao.belongsTo(Candidato, { foreignKey: 'userId' })
+
+export default () => {}
