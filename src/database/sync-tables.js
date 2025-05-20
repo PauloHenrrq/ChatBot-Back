48037eb351b07b2ae204c92639820fc97b0301f7
PH <paulofera159@gmail.com>
2025-05-19 21:45:07 -0300

    Att

--- src/database/sync-tables.js
+++ src/database/sync-tables.js
@@ -5,7 +5,6 @@ AssociationsKeys()
 
 const syncTables = async () => {
   try {
-    await DBConnection.getQueryInterface().removeColumn('Candidato', 'descricao');
     await DBConnection.sync({ alter: true })
 
     console.log('Tabelas sincronizadas com sucesso!')
